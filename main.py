a3dc3ea80988df0055a0c3028203ebf6786746e0
Mohammed ADDI <mohammed_addi@yahoo.com>
2023-02-15 21:34:59 +0100

    add realDivi function

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+# Define the functions for each operation
 def sum(a, b):
     return a + b
+
+def realDivi(a, b):
+    return a / b
+
+# Display the menu and get user input
+print("Select an operation:")
+print("1. Add")
+print("2. Divide")
+
+choice = input("Enter choice (1/2): ")
+
 # Get user input for operands
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-result = sum(num1, num2)
-print(num1, "+", num2, "=", result)
+
+# Perform the selected operation and display the result
+if choice == '1':
+    result = sum(num1, num2)
+    print(num1, "+", num2, "=", result)
+
+elif choice == '2':
+    result = realDivi(num1, num2)
+    print(num1, "/", num2, "=", result)
+
+else:
+    print("Invalid input")
+
