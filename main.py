12991200ad5297d3b4008509144bcce886cde734
Mohammed ADDI <mohammed_addi@yahoo.com>
2023-02-15 21:31:03 +0100

    add sum function

---
+++ main.py
@@ -0,0 +1,7 @@
+def sum(a, b):
+    return a + b
+# Get user input for operands
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+result = sum(num1, num2)
+print(num1, "+", num2, "=", result)
